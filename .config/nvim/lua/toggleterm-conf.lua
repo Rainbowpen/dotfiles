1b52987f11144f856bc252a2fe3f80cfa5c5f874
Simon.Z <cy-cpe940305@hotmail.com>
2022-07-07 15:20:40 +0800

    add lazygit's configure file and nvim toggleterm shortcut for that.

--- .config/nvim/lua/toggleterm-conf.lua
+++ .config/nvim/lua/toggleterm-conf.lua
@@ -5,7 +5,7 @@ require("toggleterm").setup{
   hide_numbers = true, -- hide the number column in toggleterm buffers
   shade_filetypes = {},
   shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
-  shading_factor = '3', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+  shading_factor = 1, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
   start_in_insert = true,
   insert_mappings = true, -- whether or not the open mapping applies in insert mode
   terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
@@ -20,18 +20,22 @@ require("toggleterm").setup{
     -- see :h nvim_open_win for details on borders however
     -- the 'curved' border is a custom border type
     -- not natively supported but implemented in this plugin.
-    border = 'single',
-    width = 80,
-    height = 60,
-    winblend = 3,
+    border = 'curved',
+    width = 150,
+    height = 50,
+    winblend = 0,
+    highlights = {
+      border = "Normal",
+      background = "Normal",
+    }
   }
 }
 
 
 function _G.set_terminal_keymaps()
   local opts = {noremap = true}
-  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-Space>', [[<C-\><C-n>]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
 end
@@ -40,3 +44,13 @@ end
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
 local Terminal = require("toggleterm.terminal").Terminal
+
+-- lazygit
+local lazygit = Terminal:new({cmd = "lazygit", direction = "float"})
+
+function _LAZYGIT_TOGGLE()
+	lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>a", "<cmd>ToggleTermToggleAll<CR>", {noremap = true, silent = true})
