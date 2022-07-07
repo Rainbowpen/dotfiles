e0990d54229e673f1b5ea8091eeceaa8a153832a
Simon.Z <cy-cpe940305@hotmail.com>
2022-07-07 16:29:58 +0800

    fixed neovim's galaxyline not working properly.

--- .config/nvim/lua/galaxyline/statusline.lua
+++ .config/nvim/lua/galaxyline/statusline.lua
@@ -40,18 +40,7 @@ local mode_color = function()
     end
 end
 
---[[
-gls.left[2] = {
-    statusIcon = {
-        provider = function()
-            return "   "
-        end,
-        highlight = {colors.bg, colors.blue},
-        separator = "  ",
-        separator_highlight = {colors.blue, colors.lightbg}
-    }
-}
-]]--
+
 gls.left[1] = {
     ViMode = {
         provider = function()
@@ -185,19 +174,7 @@ gls.right[1] = {
         highlight = {colors.grey, colors.bg}
     }
 }
---[[
-gls.right[2] = {
-    GitIcon = {
-        provider = function()
-            return " "
-        end,
-        condition = condition.check_git_workspace,
-        highlight = {colors.grey, colors.lightbg},
-        separator = "",
-        separator_highlight = {colors.lightbg, colors.bg}
-    }
-}
-]]--
+
 gls.right[2] = {
 	BufferType = {
         provider = 'FileTypeName',
@@ -206,15 +183,15 @@ gls.right[2] = {
         highlight = {colors.grey, colors.bg}
     }
 }
+
 gls.right[3] = {
 	 FileEncode = {
         provider = 'FileEncode',
         condition = condition.hide_in_width,
-        separator = ' ',
-        separator_highlight = {'NONE', colors.bg},
-        highlight = {colors.grey, colors.bg}
+        separator = ' ', separator_highlight = {'NONE', colors.bg}, highlight = {colors.grey, colors.bg}
     }
 }
+
 gls.right[4] = {
     GitBranch = {
         provider = "GitBranch",
@@ -232,43 +209,6 @@ gls.right[5] = {
         highlight = {colors.bg, colors.bg},
     }
 }
-
---[[
-gls.right[4] = {
-    viMode_icon = {
-        provider = function()
-            return " "
-        end,
-        highlight = {colors.bg, colors.red},
-        separator = " ",
-        separator_highlight = {colors.red, colors.lightbg}
-    }
-}
-
-gls.right[5] = {
-    ViMode = {
-        provider = function()
-            local alias = {
-                n = "Normal",
-                i = "Insert",
-                c = "Command",
-                V = "Visual",
-                [""] = "Visual",
-                v = "Visual",
-                R = "Replace"
-            }
-            local current_Mode = alias[vim.fn.mode()]
-
-            if current_Mode == nil then
-                return "  Terminal "
-            else
-                return "  " .. current_Mode .. " "
-            end
-        end,
-        highlight = {colors.red, colors.lightbg}
-    }
-}
-]]--
 
 gls.right[6] = {
     some_icon = {
@@ -290,5 +230,99 @@ gls.right[7] = {
     }
 }
 
+gls.short_line_left[1] = {
+    ViMode_right_space = {
+        provider = function()
+            return " "
+        end,
+        highlight = {colors.bg, colors.lightbg}
+    }
+}
 
+gls.short_line_left[2] = {
+    FileIcon = {
+        provider = "FileIcon",
+        condition = condition.buffer_not_empty,
+        highlight = {colors.fg, colors.lightbg}
+    }
+}
+
+gls.short_line_left[3] = {
+    FileName = {
+        provider = {"FileName"},
+        condition = condition.buffer_not_empty,
+        highlight = {colors.fg, colors.lightbg},
+        separator = " ",
+        separator_highlight = {colors.lightbg, colors.lightbg2}
+    }
+}
+
+gls.short_line_left[4] = {
+    current_dir = {
+        provider = function()
+            local dir_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+            return "  " .. dir_name .. " "
+        end,
+        highlight = {colors.grey, colors.lightbg2},
+        separator = " ",
+        separator_highlight = {colors.lightbg2, colors.bg}
+    }
+}
+
+gls.short_line_right[1] = {
+	BufferType = {
+        provider = 'FileTypeName',
+        separator = ' ',
+        separator_highlight = {'NONE', colors.bg},
+        highlight = {colors.grey, colors.bg}
+    }
+}
+
+gls.short_line_right[2] = {
+	 FileEncode = {
+        provider = 'FileEncode',
+        condition = condition.hide_in_width,
+        separator = ' ',
+        separator_highlight = {'NONE', colors.bg},
+        highlight = {colors.grey, colors.bg}
+    }
+}
+
+gls.short_line_right[3] = {
+    GitBranch = {
+        provider = "GitBranch",
+        icon = "   ",
+        condition = condition.check_git_workspace,
+        highlight = {colors.grey, colors.bg},
+    }
+}
+
+gls.short_line_right[4] = {
+    space = {
+        provider = function()
+            return " "
+        end,
+        highlight = {colors.bg, colors.bg},
+    }
+}
+
+gls.short_line_right[5] = {
+    some_icon = {
+        provider = function()
+            return " "
+        end,
+        separator = "",
+        separator_highlight = {colors.fg, colors.bg},
+        highlight = {colors.bg, colors.fg}
+    }
+}
+
+gls.short_line_right[6] = {
+    line_percentage = {
+		provider = 'LinePercent',
+        highlight = {colors.fg, colors.lightbg},
+        separator = ' ',
+        separator_highlight = {'NONE', colors.lightbg}
+    }
+}
 
